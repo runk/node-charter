6f7c1b23d82206ef38d7cadf1fded993e32f3a13
Dmitry Shirokov <deadrunk@gmail.com>
2013-11-13 19:22:30 +1100

    client code

--- README.md
+++ README.md
@@ -11,4 +11,16 @@ npm i charter
 ```
 
 
+### Example
 
+```javascript
+var Client = require('charter');
+
+var c = new Client(1);
+c.clear();
+c.names(['default']);
+c.labels(['time', 'value']);
+
+for (var i = 0; i < 100; i++)
+  c.push([i, Math.round(Math.random() * 100)]);
+```
--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// nothing here so far
+var dgram = require('dgram');
+var async = require('async');
+
+
+function Client(id, host, port) {
+  this.host = host || 'localhost';
+  this.port = (port || 2000) + id;
+
+  this.socket = dgram.createSocket('udp4')
+  this.q = async.queue(this.sender.bind(this));
+}
+
+
+Client.prototype.sender = function(message, cb) {
+  var b = new Buffer(message);
+  this.socket.send(b, 0, b.length, this.port, this.host, function(err) {
+    if (err) throw err;
+    setTimeout(cb, 5);
+  });
+};
+
+
+Client.prototype.clear = function() {
+  this.q.push('CLEAR');
+};
+
+
+Client.prototype.close = function() {
+  this.q.push('CLOSE');
+};
+
+
+Client.prototype.names = function(arr) {
+  this.q.push('NAMES ' + arr.join(' '));
+};
+
+
+Client.prototype.labels = function(arr) {
+  this.q.push('LABELS ' + arr.join(' '));
+};
+
+
+Client.prototype.push = function(arr) {
+  var msg = 's ' + arr.join(' ');
+  this.q.push(msg);
+};
+
+
+module.exports = Client;
